15d74cd1a097ae5b4a8578d95284f8d273bed74c
iarka <33841006+iarka@users.noreply.github.com>
2022-06-29 17:51:25 +0200

    Update movies.js

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,23 +1,19 @@
 import axios from 'axios'
 
 export async function getPopularMovies (page) {
-  let result
-  await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=f3fb983d1a3524887a8a8717a10b439b&language=fr-FR&page=${page}`)
+  return await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=f3fb983d1a3524887a8a8717a10b439b&language=fr-FR&page=${page}`)
     .then(response => {
-      result = response.data.results
+      return response.data.results
     }, error => {
       console.log(error)
     })
-  return result
 }
 
 export async function getTopRatedMovies () {
-  let result
-  await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=f3fb983d1a3524887a8a8717a10b439b&language=fr-FR&page=1')
+  return await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=f3fb983d1a3524887a8a8717a10b439b&language=fr-FR&page=1')
     .then(response => {
-      result = response.data.results
+      return response.data.results
     }, error => {
       console.log(error)
     })
-  return result
 }
